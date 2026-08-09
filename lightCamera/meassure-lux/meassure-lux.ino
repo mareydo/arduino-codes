0008b7749503871dd948a1bfd99611f15b276c89
brk-pnt <enable.breakpoints@gmail.com>
2026-08-09 16:00:46 +0200

    FEAT (lightCamera): interrupt manual pin read

--- lightCamera/meassure-lux/meassure-lux.ino
+++ lightCamera/meassure-lux/meassure-lux.ino
@@ -4,9 +4,9 @@
 
 #define PIN_SDA 32
 #define PIN_SCL 33
+#define PIN_INT 25
 /////////////////////////////////////////////////////////////////
 Adafruit_TSL2591 lightSensor = Adafruit_TSL2591(2591);
-/////////////////////////////////////////////////////////////////
 bool sensor_init()
 {
   if(!Wire.begin(PIN_SDA,PIN_SCL))
@@ -35,23 +35,45 @@ void sensor_printLux()
   uint32_t raw = lightSensor.getLuminosity(TSL2591_FULLSPECTRUM);
   uint16_t lb = raw & 0xFFFF;
   uint16_t ub = raw >> 16;
-  Serial.println(lightSensor.calculateLux(lb,ub));
+  //Serial.println(lightSensor.calculateLux(lb,ub));
+  Serial.println(String(lb) + " " + String(ub));
 }
 
-#define VERSION 2
+#define SENSOR_HILIM 1500
+#define SENSOR_LOLIM 100
+
+void sensor_registerInterrupt()
+{
+  lightSensor.registerInterrupt(SENSOR_LOLIM,
+                                SENSOR_HILIM,
+                                TSL2591_PERSIST_5);
+  delay(300);
+  lightSensor.enable();
+}
+/////////////////////////////////////////////////////////////////
+void esp_initINT()
+{
+  pinMode(PIN_INT, INPUT_PULLUP);
+}
+/////////////////////////////////////////////////////////////////
 void setup() 
 {
   delay(2000);
   Serial.begin(115200);
-  Serial.println(VERSION);
   sensor_init();
-
+  esp_initINT();
+  sensor_registerInterrupt();
 
 }
 
 void loop() 
 {
-  sensor_printLux();
+  //sensor_printLux();
+  if(digitalRead(PIN_INT)==LOW)
+  {
+    sensor_printLux();
+    lightSensor.clearInterrupt();
+  }
   delay(500);
 
 }
