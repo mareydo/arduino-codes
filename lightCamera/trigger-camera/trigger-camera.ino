497b479300aa66a80402233513d0c71c0a50b3a9
brk-pnt <enable.breakpoints@gmail.com>
2026-08-16 16:39:54 +0200

    FEAT: PoC camera project

---
+++ lightCamera/trigger-camera/trigger-camera.ino
@@ -0,0 +1,185 @@
+#include "esp_camera.h"
+#include "FS.h"                // SD Card ESP32
+#include "SD_MMC.h"            // SD Card ESP32
+#include "soc/soc.h"           // Disable brownout problems
+#include "soc/rtc_cntl_reg.h"  // Disable brownout problems
+#include "driver/rtc_io.h"
+#include <EEPROM.h>
+
+#define CAM_PIN_PWDN    32
+#define CAM_PIN_RESET   -1 //software reset will be performed
+#define CAM_PIN_XCLK    0
+#define CAM_PIN_SIOD    26
+#define CAM_PIN_SIOC    27
+
+#define CAM_PIN_D7      35  
+#define CAM_PIN_D6      34
+#define CAM_PIN_D5      39
+#define CAM_PIN_D4      36
+#define CAM_PIN_D3      21  //21
+#define CAM_PIN_D2      19
+#define CAM_PIN_D1      18
+#define CAM_PIN_D0       5
+#define CAM_PIN_VSYNC   25
+#define CAM_PIN_HREF    23
+#define CAM_PIN_PCLK    22
+
+#define VIDEO_DURATION_MS 5000
+#define FPS_2_MS(ms) 1000.0/ms
+
+#define DEBUG_LED
+#ifdef DEBUG_LED
+  #define DEBUG_LED_PIN 33
+#endif
+
+bool SD_connect()
+{
+  if (!SD_MMC.begin("/sdcard", true)) { // true enables 1-bit mode to free up GPIOs
+    Serial.println("SD Card Mount Failed");
+    return false;
+  }
+ 
+  uint8_t cardType = SD_MMC.cardType();
+  if(cardType == CARD_NONE){
+    Serial.println("No SD Card attached");
+    return false;
+  }
+  return true;
+}
+
+esp_err_t camera_init()
+{
+  camera_config_t camera_config = {
+    .pin_pwdn  = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sccb_sda = CAM_PIN_SIOD,
+    .pin_sccb_scl = CAM_PIN_SIOC,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG,//YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_UXGA,//QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
+
+    .jpeg_quality = 12, //0-63, for OV series camera sensors, lower number means higher quality
+    .fb_count = 1, //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
+    .grab_mode = CAMERA_GRAB_WHEN_EMPTY//CAMERA_GRAB_LATEST. Sets when buffers should be filled
+  };
+
+
+  if(CAM_PIN_PWDN != -1){
+      pinMode(CAM_PIN_PWDN, OUTPUT);
+      digitalWrite(CAM_PIN_PWDN, LOW);
+  }
+
+  //initialize the camera
+  esp_err_t err = esp_camera_init(&camera_config);
+  if (err != ESP_OK) {
+      return err;
+  }
+  #ifdef DEBUG_LED
+    pinMode(DEBUG_LED_PIN,OUTPUT);
+  #endif
+
+  return ESP_OK;
+}
+
+void camera_turnLEDOn() {
+  #ifdef DEBUG_LED
+  digitalWrite(DEBUG_LED_PIN,HIGH);
+  #endif
+}
+
+void camera_turnLEDOff() {
+  #ifdef DEBUG_LED
+  digitalWrite(DEBUG_LED_PIN,LOW);
+  #endif
+}
+
+RTC_DATA_ATTR uint8_t videoNumber = 0;
+void camera_loadVideoNumber()
+{
+  EEPROM.begin(1);
+  videoNumber = EEPROM.read(0) + 1;
+}
+
+void camera_saveVideoNumber()
+{
+  EEPROM.write(0, videoNumber);
+  EEPROM.commit();
+}
+
+void camera_recordVideo() 
+{
+  camera_fb_t * fb = NULL;
+  esp_err_t res = ESP_OK;
+    
+  String path = "/video" + String(videoNumber) + ".avi";
+  
+  fs::FS &fs = SD_MMC;
+  
+  File file = fs.open(path.c_str(), FILE_WRITE);
+  if(!file){ 
+    Serial.print("No file");
+    Serial.println(path.c_str());
+    return; 
+  }
+  
+  unsigned long startTime = millis();
+  
+  while (millis() - startTime < VIDEO_DURATION_MS) 
+  {
+    fb = esp_camera_fb_get();
+    if (!fb) { 
+      Serial.println("Cannot obtain buffer");
+      break; 
+    }
+
+    file.write(fb->buf, fb->len);
+    esp_camera_fb_return(fb);
+
+    delay(33);
+  }
+    
+  file.close();
+  return;
+}
+
+void setup() 
+{
+  delay(2000);
+  Serial.begin(115200);
+  Serial.println("Begin");
+  //camera_loadVideoNumber();
+  esp_err_t retVal = camera_init();
+  bool ret = SD_connect();
+
+  Serial.print("Inited with code");
+  Serial.println(retVal, ret);
+  camera_turnLEDOn();
+  Serial.println("START");
+  camera_recordVideo();
+  Serial.println("STOP");
+  camera_turnLEDOff();
+  //camera_saveVideoNumber();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
